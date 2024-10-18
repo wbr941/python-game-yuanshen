ae0a38b02aa4270043dff545a57f1bc14865b8ac
wangboran <wangboran@wangboran.com>
2024-10-18 19:24:37 +0800

    重构

--- src/main.py
+++ src/main.py
@@ -1,23 +1,30 @@
 import pygame
 import sys
 from setting import Setting
+class YuanShen:
+    def __init__(self):
+        pygame.init()
+        self.setting = Setting()
+        self.screen = pygame.display.set_mode((self.setting.screen_width, self.setting.screen_height))
+        self.bg_color = self.setting.bg_color
+        self.clock = pygame.time.Clock()
+        self.start_img = pygame.image.load("pic\start.png")
+        self.start_img_rect = self.start_img.get_rect()
+        self.start_img_rect.center = (0,0)
 
-pygame.init()
-setting = Setting()
-screen = pygame.display.set_mode((setting.screen_width, setting.screen_height))
-bg_color = setting.bg_color
-clock = pygame.time.Clock()
-start_img = pygame.image.load("pic\start.png")
-start_img_rect = start_img.get_rect()
-start_img_rect.center = (0,0)
-while True:
-    for event in pygame.event.get():  # 遍历所有事件
-        if event.type == pygame.QUIT:  # 如果单击关闭窗口，则退出
-            sys.exit()
-    
-    screen.fill(bg_color)
-    screen.blit(start_img,start_img_rect)
-    pygame.display.flip()
-    clock.tick(60)
-     
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():  # 遍历所有事件
+                if event.type == pygame.QUIT:  # 如果单击关闭窗口，则退出
+                    sys.exit()
+            
+        
+            self.screen.fill(self.bg_color)
+            self.screen.blit(self.start_img,self.start_img_rect)
+            pygame.display.flip()
+            self.clock.tick(60)
+
+game = YuanShen()
+game.run_game()
+        
 
