5ebd6056ed91d59d9fe56c4117892e4e1d1fea73
wangboran <wangboran@wangboran.com>
2024-10-18 19:14:04 +0800

    启动画面

--- src/main.py
+++ src/main.py
@@ -6,12 +6,16 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 700))
 bg_color = (255,255,255)
 clock = pygame.time.Clock()
+start_img = pygame.image.load("pic\start.png")
+start_img_rect = start_img.get_rect()
+start_img_rect.center = (0,0)
 while True:
     for event in pygame.event.get():  # 遍历所有事件
         if event.type == pygame.QUIT:  # 如果单击关闭窗口，则退出
             sys.exit()
     
     screen.fill(bg_color)
+    screen.blit(start_img,start_img_rect)
     pygame.display.flip()
     clock.tick(60)
      
