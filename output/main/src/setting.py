8c52372f11dfe06204a809d0dc161be6d943ad0f
wangboran <wangboran@wangboran.com>
2024-10-19 15:25:41 +0800

    更改

---
+++ output/main/src/setting.py
@@ -0,0 +1,9 @@
+class Setting:
+    """游戏设置"""
+    def __init__(self):
+        # 设置屏幕宽度
+        self.screen_width = 1280
+        # 设置屏幕高度
+        self.screen_height = 700
+        # 设置背景颜色，RGB格式
+        self.bg_color = (255, 255, 255)
